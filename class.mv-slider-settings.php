c860cd9c357c2932db322211feab00bfc2f354c4
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2023-12-14 15:32:27 -0300

    add: inputs na pagina de configuração do plugin

--- class.mv-slider-settings.php
+++ class.mv-slider-settings.php
@@ -1,7 +1,8 @@
 <?php
 
-if(!class_exists('MV_Slider_Settings')) {
-    class MV_Slider_Settings {
+if (!class_exists('MV_Slider_Settings')) {
+    class MV_Slider_Settings
+    {
         // Para acessar esse atributo externamente sem precisar instanciar a classe
         public static $options;
 
@@ -16,11 +17,24 @@ if(!class_exists('MV_Slider_Settings')) {
         // Criando as seções
         public function admin_init()
         {
+
+            register_setting(
+                'mv_slider_group',
+                'mv_slider_options',
+            );
+
             add_settings_section(
                 'mv_slider_main_section',
                 'How does it work?',
                 null,
                 'mv_slider_page1'
+            );
+
+            add_settings_section(
+                'mv_slider_second_section',
+                'Other Plugin Options',
+                null,
+                'mv_slider_page2'
             );
 
             add_settings_field(
@@ -30,12 +44,68 @@ if(!class_exists('MV_Slider_Settings')) {
                 'mv_slider_page1',
                 'mv_slider_main_section',
             );
+
+            add_settings_field(
+                'mv_slider_title',
+                'Slider Title',
+                array($this, 'mv_slider_title_callback'),
+                'mv_slider_page2',
+                'mv_slider_second_section',
+            );
+
+            add_settings_field(
+                'mv_slider_bullets',
+                'Display Bullets',
+                array($this, 'mv_slider_bullets_callback'),
+                'mv_slider_page2',
+                'mv_slider_second_section',
+            );
+
+            add_settings_field(
+                'mv_slider_style',
+                'Slider Style',
+                array($this, 'mv_slider_style_callback'),
+                'mv_slider_page2',
+                'mv_slider_second_section',
+            );
         }
 
         public function mv_slider_shortcode_callback()
         {
+        ?>
+            <span>Use the shortcode [mv_slider] to display the slider in any page/post/widget</span>
+        <?php
+        }
+
+        public function mv_slider_title_callback()
+        {
             ?>
-                <span>Use the shortcode [mv_slider] to display the slider in any page/post/widget</span>
+                <input type="text" id="mv_slider_title" name="mv_slider_options[mv_slider_title]" value="<?php echo isset(self::$options['mv_slider_title']) ? esc_attr(self::$options['mv_slider_title']) : ''; ?>">
+            <?php
+        }
+
+        public function mv_slider_bullets_callback()
+        {
+            ?>
+                <input type="checkbox" id="mv_slider_bullets" name="mv_slider_options[mv_slider_bullets]" value="1" <?php 
+                if(isset(self::$options['mv_slider_bullets'])) {
+                    checked(1, self::$options['mv_slider_bullets'], true); 
+                }
+                ?>/>
+                <label for ="mv_slider_bullets">Whether to display bullets or not</label>
+            <?php
+        }
+
+        public function mv_slider_style_callback(){
+            ?>
+            <select 
+                id="mv_slider_style" 
+                name="mv_slider_options[mv_slider_style]">
+                <option value="style-1" 
+                    <?php isset( self::$options['mv_slider_style'] ) ? selected( 'style-1', self::$options['mv_slider_style'], true ) : ''; ?>>Style-1</option>
+                <option value="style-2" 
+                    <?php isset( self::$options['mv_slider_style'] ) ? selected( 'style-2', self::$options['mv_slider_style'], true ) : ''; ?>>Style-2</option>
+            </select>
             <?php
         }
     }
--- views/settings-page.php
+++ views/settings-page.php
@@ -5,7 +5,13 @@
     </h1>
     <!-- O Wordpress já oferece o options.php para salvar o form -->
     <!-- É possível verificar os options acessando /wp-admin/options.php -->
-    <form action="option.php" method="post">
-
+    <form action="options.php" method="post">
+    <?php
+        settings_fields('mv_slider_group');
+        // Exibe as abas de configurações
+        do_settings_sections('mv_slider_page1');
+        do_settings_sections('mv_slider_page2');
+        submit_button('Save Settings');
+    ?>
     </form>
 </div>
